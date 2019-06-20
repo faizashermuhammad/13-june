aa2018e6a5b124eb6e5880a8f6b374cfa2f1b6a6
faizashermuhammad <49517190+faizashermuhammad@users.noreply.github.com>
2019-06-20 17:54:03 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -13,9 +13,39 @@
 //     console.log("please login")
 // }
 
-let temprature = 50;
-if (temprature > 40) {
-    alert("uit closed");
-} else {
-     alert("important announcement");
+// let temprature = 50;
+// if (temprature > 40) {
+//     alert("uit closed");
+// } else {
+//      alert("important announcement");
+// }
+// let amount=1000000;
+
+// if( amount <=1000000) {
+//     alert("Congratulations! You are a millionaire");
+// } else{
+//     alert("I'm sorry, you are not a millionaire");
+// }
+// let isCarElectric=true;
+// if(isCarElectric) {
+//     alert("manufactured by Tesla");
+// } else { 
+//     alert("not");
+// }
+// let highScore= 90;
+// let currentScore=60;
+// if(highScore<currentScore) {
+//   alert("congratulation user,s heigh score is"+currentScore);
+// } else{
+//     alert("Try Again. You just need "+ highScore + "too win");
+// }
+function myFunction(){
+    document.querySelector("button").innereHTML
+
+let number=20;
+if(number%2==0) {
+    alert("This is EVEN");
+} else{
+    alert("This is ODD");
+}
 }
