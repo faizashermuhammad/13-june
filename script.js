cc880d92b4d6ab3426324262c985c9ba79d8ac9e
faizashermuhammad <49517190+faizashermuhammad@users.noreply.github.com>
2019-06-20 15:55:39 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// let speed = 90;
+
+// if (speed > 60) {
+//     console.log("you are overspeeding")
+// } else {
+//     console.log("not");
+// // }
+// let isLogin="true";
+
+// if (isLogin){
+//     console.log("welcome")
+// } else{
+//     console.log("please login")
+// }
+
+let temprature = 50;
+if (temprature > 40) {
+    alert("uit closed");
+} else {
+     alert("important announcement");
+}
